1ff74895443b32ddbd30d0bbe4d1c5260f373f52
Paul Tobin <tobster111@gmail.com>
2025-12-24 10:43:40 +0000

    Add screen dimension output to main function

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import pygame
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 
 def main():
     print("Starting Asteroids with pygame version: VERSION =", pygame.version.ver)
+    print(f"Screen dimensions: WIDTH = {SCREEN_WIDTH}, HEIGHT = {SCREEN_HEIGHT}")
 
 
 if __name__ == "__main__":
