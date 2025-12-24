d439e34a3f64ce524368c390221fd88112bc309c
Paul Tobin <tobster111@gmail.com>
2025-12-24 11:04:44 +0000

    Refactor main function to set up game clock and frame rate limiting

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygame.display.set_caption("Asteroids")
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         for event in pygame.event.get():
@@ -17,6 +20,10 @@ def main():
         log_state()
         screen.fill("black")
         pygame.display.flip()
+
+        # limit to 60 FPS
+        dt = clock.tick(60) / 1000
+
 
 
 if __name__ == "__main__":
