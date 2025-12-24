e3a914839a7ba0234bd90f6447a67ab1fb1d402f
Paul Tobin <tobster111@gmail.com>
2025-12-24 10:46:47 +0000

    Refactor screen dimension output for clarity in main function

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 
 def main():
     print("Starting Asteroids with pygame version: VERSION =", pygame.version.ver)
-    print(f"Screen dimensions: WIDTH = {SCREEN_WIDTH}, HEIGHT = {SCREEN_HEIGHT}")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
 
 
 if __name__ == "__main__":
