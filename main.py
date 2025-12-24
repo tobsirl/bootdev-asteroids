4d51fb9394c1a4a7469c5ad52d4f5e4a709af956
Paul Tobin <tobster111@gmail.com>
2025-12-24 10:55:08 +0000

    Refactor main function to initialize pygame and handle events

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 import pygame
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+from logger import log_state
 
 def main():
     print("Starting Asteroids with pygame version: VERSION =", pygame.version.ver)
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                return
+        log_state()
+        screen.fill("black")
+        pygame.display.flip()
 
 
 if __name__ == "__main__":
